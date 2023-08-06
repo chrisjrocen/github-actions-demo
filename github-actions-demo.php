c9cc28259c08d3127c584e2f1f9dde8ecad67a1b
chrisjrocen <chrisocen4@gmail.com>
2023-08-06 12:29:14 +0300

    changed text

--- github-actions-demo.php
+++ github-actions-demo.php
@@ -8,7 +8,14 @@
 
 // Alter WooCommerce Billing Details Text.
 add_filter( 'gettext', function ( $translated_text ) {
-	if ( 'my awesome Services' === $translated_text ) {
+	if ( strtolower('my awesome Services') === strtolower($translated_text) ) {
+		$translated_text = 'What I do';
+	}
+	return $translated_text;
+});
+
+add_filter( 'gettext', function ( $translated_text ) {
+	if ( strtolower('My Awesome Services') === strtolower($translated_text) ) {
 		$translated_text = 'What I do';
 	}
 	return $translated_text;
