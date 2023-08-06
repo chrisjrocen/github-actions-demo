5fd6bf365c5b87c24ff14fb16757c3ad06a23dfd
chrisjrocen <chrisocen4@gmail.com>
2023-08-06 13:40:00 +0300

    addding code to website

--- github-actions-demo.php
+++ github-actions-demo.php
@@ -1,25 +1,10 @@
 <?php 
 /**
- * Plugin Name: Wordcamp Kla
+ * Plugin Name: Wordcamp Kla demo
  * Description: Demo for Github actions
  * Version: 1.0.0
  * Author: chris
  */
-
-// Alter WooCommerce Billing Details Text.
-add_filter( 'gettext', function ( $translated_text ) {
-	if ( strtolower('my awesome Services') === strtolower($translated_text) ) {
-		$translated_text = 'What I do';
-	}
-	return $translated_text;
-});
-
-add_filter( 'gettext', function ( $translated_text ) {
-	if ( strtolower('My Awesome Services') === strtolower($translated_text) ) {
-		$translated_text = 'What I do';
-	}
-	return $translated_text;
-});
 
 
 //Enqueue styles and scripts
