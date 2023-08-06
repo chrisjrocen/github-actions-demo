65243eec969cc4f55ef1487e9b09340a3485c045
chrisjrocen <chrisocen4@gmail.com>
2023-08-06 12:39:41 +0300

    css

--- github-actions-demo.php
+++ github-actions-demo.php
@@ -20,3 +20,12 @@ add_filter( 'gettext', function ( $translated_text ) {
 	}
 	return $translated_text;
 });
+
+
+//Enqueue styles and scripts
+add_action('wp_enqueue_scripts', 'jeff_plugin_enqueue_scripts');
+function jeff_plugin_enqueue_scripts()
+{
+	wp_enqueue_style('Main styles file', plugin_dir_url( __FILE__ ) . 'style.css', array(), 0.1, 'all');
+
+}
